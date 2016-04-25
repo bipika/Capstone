7e458625a9d0fd93a13607b4d1778b7e4d4b49cf
bipika <biamatya@veriskhealth.com>
2016-04-25 14:33:59 +0545

    final 1

---
+++ SHINY/README.md
@@ -0,0 +1,12 @@
+nlp-word-prediction
+===================
+
+Coursera / Johns Hopkins Data Science capstone project
+
+This is a shiny app that predicts probable next words from two or three words
+that are typed by the user.
+The app uses a set of 4grams and frequencies processed from a big set of
+ excerpts from Twitter, blogs, and news.
+
+THis is a test of Jenkins.
+1 2 3 4
---
+++ SHINY/predict/ui.R
@@ -0,0 +1,24 @@
+shinyUI(fluidPage(
+    titlePanel("Word Prediction"),
+    mainPanel(
+        width=6
+        ,uiOutput("topPanel")
+        ,p()
+        ,tags$textarea(id="text1", rows=6, cols=80, "Give it a ")
+        ,p()
+        ,uiOutput("bottomPanel")
+        ,p()
+        ,selectInput("rand_bab", "Top prediction only:",
+                    choices = c(TRUE, FALSE))
+        ,numericInput(inputId="num_bab", label="Number of words to predict:",
+                      value=4,min=1,max=100)
+        
+    ),
+    sidebarPanel(
+        width=6
+        ,tabsetPanel(id = "explanation", position="above"
+            ,tabPanel("Prediction", includeMarkdown("readme_prediction.Rmd"))
+            ,tabPanel("Processing the Data", includeMarkdown("readme_processing.Rmd"))
+        )
+    )
+))
